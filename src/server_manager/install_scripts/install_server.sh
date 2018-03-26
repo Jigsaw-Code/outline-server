8b7ece74b35c6556ba9a70eff90000637749dae1
fortuna <fortuna@users.noreply.github.com>
2018-03-26 18:05:42 -0400

    Add curl comment

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -181,6 +181,8 @@ function start_watchtower() {
 
 # Waits for Shadowbox to be up and healthy
 function wait_shadowbox() {
+  # We use insecure connection because our threat model doesn't include localhost port
+  # interception and our certificate doesn't have localhost as a subject alternative name
   until curl --insecure -s "${LOCAL_API_URL}/access-keys" >/dev/null; do sleep 1; done
 }
 
