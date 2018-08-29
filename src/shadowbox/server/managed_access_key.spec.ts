3b8e92ad22781bd6d54dcffcf44d334ead2bd8bb
fortuna <fortuna@users.noreply.github.com>
2018-08-29 18:06:15 -0400

    Rename managed_user to managed_access_key

--- src/shadowbox/server/managed_access_key.spec.ts
+++ src/shadowbox/server/managed_access_key.spec.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { createManagedAccessKeyRepository } from './managed_user';
+import { createManagedAccessKeyRepository } from './managed_access_key';
 import { MockShadowsocksServer, MockStats, InMemoryFile } from './mocks/mocks';
 import { AccessKeyRepository } from '../model/access_key';
 
