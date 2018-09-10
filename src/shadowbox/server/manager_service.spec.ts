76c0a94c29d7009f06340a949dc51a9ed9e8f525
fortuna <fortuna@users.noreply.github.com>
2018-09-10 15:40:11 -0400

    Group tests

--- src/shadowbox/server/manager_service.spec.ts
+++ src/shadowbox/server/manager_service.spec.ts
@@ -35,128 +35,106 @@ describe('ShadowsocksManagerService', () => {
     expect(responseProcessed).toEqual(true);
   });
 
-  it('Return default name by default', (done) => {
-    const repo = new MockAccessKeyRepository();
-    const serverConfig = new InMemoryConfig({} as ServerConfigJson);
-    const service = new ShadowsocksManagerService('default name', serverConfig, repo, null);
-    service.getServer(
-        {params: {}}, {
-          send: (httpCode, data: ServerInfo) => {
-            expect(httpCode).toEqual(200);
-            expect(data.name).toEqual('default name');
-            responseProcessed = true;
-          }
-        },
-        done);
-  });
-
-  it('Rename changes the server name', (done) => {
-    const repo = new MockAccessKeyRepository();
-    const serverConfig = new InMemoryConfig({} as ServerConfigJson);
-    const service = new ShadowsocksManagerService('default name', serverConfig, repo, null);
-    service.renameServer(
-        {params: {name: 'new name'}}, {
-          send: (httpCode, _) => {
-            expect(httpCode).toEqual(204);
-            expect(serverConfig.written.name).toEqual('new name');
-            responseProcessed = true;
-          }
-        },
-        done);
-  });
-
-  it('lists access keys in order', (done) => {
-    const repo = new MockAccessKeyRepository();
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
-
-    // Create 2 access keys with names.
-    Promise
-        .all([
-          createNewAccessKeyWithName(repo, 'keyName1'), createNewAccessKeyWithName(repo, 'keyName2')
-        ])
-        .then((keys) => {
-          // Verify that response returns keys in correct order with correct names.
-          const res = {
-            send: (httpCode, data) => {
+  describe('getServer', () => {
+    it('Return default name if name is absent', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const serverConfig = new InMemoryConfig({} as ServerConfigJson);
+      const service = new ShadowsocksManagerService('default name', serverConfig, repo, null);
+      service.getServer(
+          {params: {}}, {
+            send: (httpCode, data: ServerInfo) => {
               expect(httpCode).toEqual(200);
-              expect(data.accessKeys.length).toEqual(2);
-              expect(data.accessKeys[0].name).toEqual(keys[0].name);
-              expect(data.accessKeys[0].id).toEqual(keys[0].id);
-              expect(data.accessKeys[1].name).toEqual(keys[1].name);
-              expect(data.accessKeys[1].id).toEqual(keys[1].id);
-              responseProcessed = true;  // required for afterEach to pass.
+              expect(data.name).toEqual('default name');
+              responseProcessed = true;
             }
-          };
-          service.listAccessKeys({params: {}}, res, done);
-        });
-  });
-
-  it('creates keys', (done) => {
-    const repo = new MockAccessKeyRepository();
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
-
-    // Verify that response returns a key with the expected properties.
-    const res = {
-      send: (httpCode, data) => {
-        expect(httpCode).toEqual(201);
-        const expectedProperties = ['id', 'name', 'password', 'port', 'method', 'accessUrl'];
-        expect(Object.keys(data).sort()).toEqual(expectedProperties.sort());
-        responseProcessed = true;  // required for afterEach to pass.
-      }
-    };
-    service.createNewAccessKey({params: {}}, res, done);
-  });
-
-  it('removes keys', (done) => {
-    const repo = new MockAccessKeyRepository();
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
-
-    // Create 2 access keys with names.
-    Promise
-        .all([
-          createNewAccessKeyWithName(repo, 'keyName1'), createNewAccessKeyWithName(repo, 'keyName2')
-        ])
-        .then((keys) => {
-          const res = {
-            send: (httpCode, data) => {
-              expect(httpCode).toEqual(204);
-              // expect that the only remaining key is the 2nd key we created.
-              expect(getFirstAccessKey(repo).id === keys[1].id);
-              responseProcessed = true;  // required for afterEach to pass.
+          },
+          done);
+    });
+    it('Return saved name', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const serverConfig = new InMemoryConfig({name: 'Server'} as ServerConfigJson);
+      const service = new ShadowsocksManagerService('default name', serverConfig, repo, null);
+      service.getServer(
+          {params: {}}, {
+            send: (httpCode, data: ServerInfo) => {
+              expect(httpCode).toEqual(200);
+              expect(data.name).toEqual('Server');
+              responseProcessed = true;
             }
-          };
-          // remove the 1st key.
-          service.removeAccessKey({params: {id: keys[0].id}}, res, done);
-        });
-  });
-
-  it('renames keys', (done) => {
-    const repo = new MockAccessKeyRepository();
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
-    const OLD_NAME = 'oldName';
-    const NEW_NAME = 'newName';
-
-    createNewAccessKeyWithName(repo, OLD_NAME).then((key) => {
-      expect(getFirstAccessKey(repo).name === OLD_NAME);
-      const res = {
-        send: (httpCode, data) => {
-          expect(httpCode).toEqual(204);
-          expect(getFirstAccessKey(repo).name === NEW_NAME);
-          responseProcessed = true;  // required for afterEach to pass.
-        }
-      };
-      service.renameAccessKey({params: {id: key.id, name: NEW_NAME}}, res, done);
+          },
+          done);
     });
   });
 
-  it('Rename returns a 500 when the repository throws an exception', (done) => {
-    const repo = new MockAccessKeyRepository();
-    spyOn(repo, 'renameAccessKey').and.throwError('cannot write to disk');
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
+  describe('renameServer', () => {
+    it('Rename changes the server name', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const serverConfig = new InMemoryConfig({} as ServerConfigJson);
+      const service = new ShadowsocksManagerService('default name', serverConfig, repo, null);
+      service.renameServer(
+          {params: {name: 'new name'}}, {
+            send: (httpCode, _) => {
+              expect(httpCode).toEqual(204);
+              expect(serverConfig.written.name).toEqual('new name');
+              responseProcessed = true;
+            }
+          },
+          done);
+    });
+  });
 
-    createNewAccessKeyWithName(repo, 'oldName').then((key) => {
+  describe('listAccessKeys', () => {
+    it('lists access keys in order', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
+
+      // Create 2 access keys with names.
+      Promise
+          .all([
+            createNewAccessKeyWithName(repo, 'keyName1'),
+            createNewAccessKeyWithName(repo, 'keyName2')
+          ])
+          .then((keys) => {
+            // Verify that response returns keys in correct order with correct names.
+            const res = {
+              send: (httpCode, data) => {
+                expect(httpCode).toEqual(200);
+                expect(data.accessKeys.length).toEqual(2);
+                expect(data.accessKeys[0].name).toEqual(keys[0].name);
+                expect(data.accessKeys[0].id).toEqual(keys[0].id);
+                expect(data.accessKeys[1].name).toEqual(keys[1].name);
+                expect(data.accessKeys[1].id).toEqual(keys[1].id);
+                responseProcessed = true;  // required for afterEach to pass.
+              }
+            };
+            service.listAccessKeys({params: {}}, res, done);
+          });
+    });
+  });
+
+  describe('createNewAccessKey', () => {
+    it('creates keys', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
+
+      // Verify that response returns a key with the expected properties.
+      const res = {
+        send: (httpCode, data) => {
+          expect(httpCode).toEqual(201);
+          const expectedProperties = ['id', 'name', 'password', 'port', 'method', 'accessUrl'];
+          expect(Object.keys(data).sort()).toEqual(expectedProperties.sort());
+          responseProcessed = true;  // required for afterEach to pass.
+        }
+      };
+      service.createNewAccessKey({params: {}}, res, done);
+    });
+    it('Create returns a 500 when the repository throws an exception', (done) => {
+      const repo = new MockAccessKeyRepository();
+      spyOn(repo, 'createNewAccessKey').and.throwError('cannot write to disk');
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
+
       const res = {send: (httpCode, data) => {}};
-      service.renameAccessKey({params: {id: key.id, name: 'newName'}}, res, (error) => {
+      service.createNewAccessKey({params: {}}, res, (error) => {
         expect(error.statusCode).toEqual(500);
         responseProcessed = true;  // required for afterEach to pass.
         done();
@@ -164,31 +142,78 @@ describe('ShadowsocksManagerService', () => {
     });
   });
 
-  it('Create returns a 500 when the repository throws an exception', (done) => {
-    const repo = new MockAccessKeyRepository();
-    spyOn(repo, 'createNewAccessKey').and.throwError('cannot write to disk');
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
+  describe('removeAccessKey', () => {
+    it('removes keys', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
 
-    const res = {send: (httpCode, data) => {}};
-    service.createNewAccessKey({params: {}}, res, (error) => {
-      expect(error.statusCode).toEqual(500);
-      responseProcessed = true;  // required for afterEach to pass.
-      done();
+      // Create 2 access keys with names.
+      Promise
+          .all([
+            createNewAccessKeyWithName(repo, 'keyName1'),
+            createNewAccessKeyWithName(repo, 'keyName2')
+          ])
+          .then((keys) => {
+            const res = {
+              send: (httpCode, data) => {
+                expect(httpCode).toEqual(204);
+                // expect that the only remaining key is the 2nd key we created.
+                expect(getFirstAccessKey(repo).id === keys[1].id);
+                responseProcessed = true;  // required for afterEach to pass.
+              }
+            };
+            // remove the 1st key.
+            service.removeAccessKey({params: {id: keys[0].id}}, res, done);
+          });
+    });
+    it('Remove returns a 500 when the repository throws an exception', (done) => {
+      const repo = new MockAccessKeyRepository();
+      spyOn(repo, 'removeAccessKey').and.throwError('cannot write to disk');
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
+
+      // Create 2 access keys with names.
+      createNewAccessKeyWithName(repo, 'keyName1').then((key) => {
+        const res = {send: (httpCode, data) => {}};
+        service.removeAccessKey({params: {id: key.id}}, res, (error) => {
+          expect(error.statusCode).toEqual(500);
+          responseProcessed = true;  // required for afterEach to pass.
+          done();
+        });
+      });
     });
   });
 
-  it('Remove returns a 500 when the repository throws an exception', (done) => {
-    const repo = new MockAccessKeyRepository();
-    spyOn(repo, 'removeAccessKey').and.throwError('cannot write to disk');
-    const service = new ShadowsocksManagerService('default name', null, repo, null);
+  describe('renameAccessKey', () => {
+    it('renames keys', (done) => {
+      const repo = new MockAccessKeyRepository();
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
+      const OLD_NAME = 'oldName';
+      const NEW_NAME = 'newName';
 
-    // Create 2 access keys with names.
-    createNewAccessKeyWithName(repo, 'keyName1').then((key) => {
-      const res = {send: (httpCode, data) => {}};
-      service.removeAccessKey({params: {id: key.id}}, res, (error) => {
-        expect(error.statusCode).toEqual(500);
-        responseProcessed = true;  // required for afterEach to pass.
-        done();
+      createNewAccessKeyWithName(repo, OLD_NAME).then((key) => {
+        expect(getFirstAccessKey(repo).name === OLD_NAME);
+        const res = {
+          send: (httpCode, data) => {
+            expect(httpCode).toEqual(204);
+            expect(getFirstAccessKey(repo).name === NEW_NAME);
+            responseProcessed = true;  // required for afterEach to pass.
+          }
+        };
+        service.renameAccessKey({params: {id: key.id, name: NEW_NAME}}, res, done);
+      });
+    });
+    it('Rename returns a 500 when the repository throws an exception', (done) => {
+      const repo = new MockAccessKeyRepository();
+      spyOn(repo, 'renameAccessKey').and.throwError('cannot write to disk');
+      const service = new ShadowsocksManagerService('default name', null, repo, null);
+
+      createNewAccessKeyWithName(repo, 'oldName').then((key) => {
+        const res = {send: (httpCode, data) => {}};
+        service.renameAccessKey({params: {id: key.id, name: 'newName'}}, res, (error) => {
+          expect(error.statusCode).toEqual(500);
+          responseProcessed = true;  // required for afterEach to pass.
+          done();
+        });
       });
     });
   });
