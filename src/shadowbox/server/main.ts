7ee325a99700201958eac3b8d7ed200a8cd40ca9
trevj <trevj@users.noreply.github.com>
2018-11-05 12:57:40 -0500

    increase go server rollout to 50% (#304)

--- src/shadowbox/server/main.ts
+++ src/shadowbox/server/main.ts
@@ -162,7 +162,7 @@ async function main() {
 
   const rollouts = createRolloutTracker(serverConfig);
   let shadowsocksServer: ShadowsocksServer;
-  if (rollouts.isRolloutEnabled('outline-ss-server', 30)) {
+  if (rollouts.isRolloutEnabled('outline-ss-server', 50)) {
     const ssMetricsLocation = `localhost:${ssMetricsPort}`;
     logging.info(`outline-ss-server metrics is at ${ssMetricsLocation}`);
     prometheusConfigJson.scrape_configs.push(
