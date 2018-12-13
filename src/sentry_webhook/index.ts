ede5235e0b8f6dac62a6052443db8958a606d550
alalamav <alalamav@users.noreply.github.com>
2018-12-12 19:01:10 -0500

    Implement cloud function to post Sentry events to Salesforce

---
+++ src/sentry_webhook/index.ts
@@ -0,0 +1,47 @@
+// Copyright 2018 The Outline Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as sentry from '@sentry/types';
+import * as express from 'express';
+
+import {postSentryEventToSalesforce} from './post_sentry_event_to_salesforce';
+
+exports.postSentryEventToSalesforce = (req: express.Request, res: express.Response) => {
+  if (req.method !== 'POST') {
+    res.status(405).send('Method not allowed');
+    return;
+  }
+  if (!req.body) {
+    res.status(400).send('Missing request body');
+    return;
+  }
+
+  const sentryEvent: sentry.SentryEvent = req.body.event;
+  if (!sentryEvent) {
+    res.status(400).send('Missing Sentry event');
+    return;
+  }
+  // Use the request message if SentryEvent.message is unpopulated.
+  sentryEvent.message = sentryEvent.message || req.body.message;
+  postSentryEventToSalesforce(sentryEvent, req.body.project)
+      .then(() => {
+        res.status(200).send();
+      })
+      .catch((e) => {
+        console.error(e);
+        // Send an OK response to Sentry - they don't need to know about errors with posting to
+        // Salesforce.
+        res.status(200).send();
+      });
+};
---
+++ src/sentry_webhook/post_sentry_event_to_salesforce.ts
@@ -0,0 +1,168 @@
+// Copyright 2018 The Outline Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as sentry from '@sentry/types';
+import * as https from 'https';
+
+// Defines the Salesforce form field names.
+interface SalesforceFormFields {
+  orgId: string;
+  recordType: string;
+  email: string;
+  description: string;
+  category: string;
+  cloudProvider: string;
+  sentryEventId: string;
+  os: string;
+  version: string;
+  type: string;
+}
+
+// Defines the Salesforce form values.
+interface SalesforceFormValues {
+  orgId: string;
+  recordType: string;
+}
+
+const SALESFORCE_DEV_HOST = 'google-jigsaw--uat.my.salesforce.com';
+const SALESFORCE_PROD_HOST = 'webto.salesforce.com';
+const SALESFORCE_PATH = '/servlet/servlet.WebToCase';
+const SALESFORCE_FORM_FIELDS_DEV: SalesforceFormFields = {
+  orgId: 'orgid',
+  recordType: 'recordType',
+  email: 'email',
+  description: 'description',
+  category: '00N3F000002Rqho',
+  cloudProvider: '00N3F000002Rqhs',
+  sentryEventId: '00N3F000002Rqhq',
+  os: '00N3F000002cLcN',
+  version: '00N3F000002cLcI',
+  type: 'type'
+};
+const SALESFORCE_FORM_FIELDS_PROD: SalesforceFormFields = {
+  orgId: 'orgid',
+  recordType: 'recordType',
+  email: 'email',
+  description: 'description',
+  category: '00N0b00000BqOA2',
+  cloudProvider: '00N0b00000BqOA7',
+  sentryEventId: '00N0b00000BqOA4',
+  os: '00N0b00000BqOfW',
+  version: '00N0b00000BqOfR',
+  type: 'type'
+};
+const SALESFORCE_FORM_VALUES_DEV: SalesforceFormValues = {
+  orgId: '00D3F000000DDDH',
+  recordType: '0123F000000MWTS',
+};
+const SALESFORCE_FORM_VALUES_PROD: SalesforceFormValues = {
+  orgId: '00D0b000000BrsN',
+  recordType: '0120b0000006e8i',
+};
+
+// Posts a Sentry event to Salesforce using predefined form data. Only posts if the event contains
+// an email address.
+export function postSentryEventToSalesforce(
+    event: sentry.SentryEvent, project: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (!event.user || !event.user.email) {
+      reject(new Error('Not posting event without email'));
+      return;
+    }
+    // Sentry development projects are marked with 'dev', i.e. outline-client-dev.
+    const isProd = project.indexOf('-dev') === -1;
+    const salesforceHost = isProd ? SALESFORCE_PROD_HOST : SALESFORCE_DEV_HOST;
+    const formFields = isProd ? SALESFORCE_FORM_FIELDS_PROD : SALESFORCE_FORM_FIELDS_DEV;
+    const formValues = isProd ? SALESFORCE_FORM_VALUES_PROD : SALESFORCE_FORM_VALUES_DEV;
+    const isClient = project.indexOf('client') !== -1;
+    const formData =
+        getSalesforceFormData(formFields, formValues, event, event.user.email, isClient);
+    const req = https.request(
+        {
+          host: salesforceHost,
+          path: SALESFORCE_PATH,
+          protocol: 'https:',
+          method: 'post',
+          headers: {
+            // The production server will reject requests that do not specify this content type.
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        },
+        (res) => {
+          if (res.statusCode === 200) {
+            resolve();
+          } else {
+            reject(new Error(`Failed to post form data, response status: ${res.statusCode}`));
+          }
+        });
+    req.on('error', (err) => {
+      reject(new Error(
+          `Failed to submit form: ${err}`,
+          ));
+    });
+    req.write(formData);
+    req.end();
+  });
+}
+
+// Returns a URL-encoded string with the Salesforce form data.
+function getSalesforceFormData(
+    formFields: SalesforceFormFields, formValues: SalesforceFormValues, event: sentry.SentryEvent,
+    email: string, isClient: boolean): string {
+  const form = [];
+  form.push(encodeFormData(formFields.orgId, formValues.orgId));
+  form.push(encodeFormData(formFields.recordType, formValues.recordType));
+  form.push(encodeFormData(formFields.email, email));
+  form.push(encodeFormData(formFields.sentryEventId, getStringValueOrEmtpy(event.event_id)));
+  form.push(encodeFormData(formFields.description, getStringValueOrEmtpy(event.message)));
+  form.push(encodeFormData(formFields.type, isClient ? 'Outline client' : 'Outline manager'));
+  const tags = getTagsMap(event.tags);
+  if (!!tags) {
+    form.push(encodeFormData(formFields.category, getStringValueOrEmtpy(tags.get('category'))));
+    form.push(encodeFormData(formFields.os, getStringValueOrEmtpy(tags.get('os.name'))));
+    form.push(
+        encodeFormData(formFields.version, getStringValueOrEmtpy(tags.get('sentry:release'))));
+    if (!isClient) {
+      form.push(encodeFormData(
+          formFields.cloudProvider, getStringValueOrEmtpy(tags.get('cloudProvider'))));
+    }
+  }
+  return form.join('&');
+}
+
+function encodeFormData(field: string, value: string) {
+  return `${encodeURIComponent(field)}=${encodeURIComponent(value)}`;
+}
+
+function getStringValueOrEmtpy(str?: string): string {
+  return str || '';
+}
+
+// Although SentryEvent.tags is declared as an index signature object, it is actually an array of
+// arrays i.e. [['key0': 'value0'], ['key1': 'value1']]. Converts the tags to a Map of strings.
+function getTagsMap(tags?: {[key: string]: string}) {
+  if (!tags) {
+    return null;
+  }
+  const tagsMap = new Map<string, string>();
+  for (const i of Object.keys(tags)) {
+    try {
+      const tagEntry = tags[i];
+      tagsMap.set(tagEntry[0], tagEntry[1]);
+    } catch (e) {
+      console.error('Failed to process tag entry');
+    }
+  }
+  return tagsMap;
+}
