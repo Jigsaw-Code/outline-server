fba31a21b09fe0d5f1069d91a9b5007b043ac612
Jon Cohen <cohenjon@google.com>
2020-01-14 15:23:26 -0500

    Make sure that we deal with lag between the changeover of the current month and release of new versions of the DB-IP database

--- src/shadowbox/scripts/update_mmdb.sh
+++ src/shadowbox/scripts/update_mmdb.sh
@@ -5,10 +5,24 @@
 
 TMPDIR="$(mktemp -d)"
 FILENAME="dbip-country-lite.mmdb"
+
+# We need to make sure that we grab an existing database at install-time
+monthdelta="0"
 ADDRESS="https://download.db-ip.com/free/dbip-country-lite-$(date +%Y-%m).mmdb.gz"
-LIBDIR="/var/lib/libmaxminddb"
+while true; do
+    curl --fail --silent --head "${ADDRESS}" > /dev/null && break
+    monthdelta=$((monthdelta+1))
+    if [[ $monthdelta -gt 10 ]]; then
+        # A weird exit code on purpose -- we should catch this long before it triggers
+        exit 2
+    fi
+    newdate=$(date --date="-$monthdelta month" +%Y-%m)
+    ADDRESS="https://download.db-ip.com/free/dbip-country-lite-${newdate}.mmdb.gz"
+done
+
 curl --silent -f "${ADDRESS}" -o "$TMPDIR/$FILENAME.gz" || exit $?
 gunzip "$TMPDIR/$FILENAME.gz"
+LIBDIR="/var/lib/libmaxminddb"
 mkdir -p $LIBDIR
 mv -f "$TMPDIR/$FILENAME" $LIBDIR
 rmdir $TMPDIR
