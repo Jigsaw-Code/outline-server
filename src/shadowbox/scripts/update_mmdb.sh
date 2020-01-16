db6aba33b2c6cf734f949698d2eede59034683f3
Jonathan Cohen <cohenjon@google.com>
2020-01-16 18:08:29 -0500

    Use DB-IP's Geolocation database instead of MaxMind's (#557)
    * Use DB-IP's free IP-to-Country database instead of MaxMind's
    
    This avoids the necesseity to either (1) bundle MaxMind's database in
    our docker image or (2) require server admins to individually create
    MaxMind accounts due to the new requirements for using GeoLite2
    databases.  See https://blog.maxmind.com/2019/12/18/significant-changes-to-accessing-and-using-geolite2-databases/
    
    * Add attribution to DB-IP for the GeoIP database
    
    * Make sure that we deal with lag between the changeover of the current
    month and release of new versions of the DB-IP database
    
    * Respond to review comments
    
    * Attribute DB-IP in the update script
    
    * Respond to review comments
    
    * Respond to review comments

--- src/shadowbox/scripts/update_mmdb.sh
+++ src/shadowbox/scripts/update_mmdb.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-# Download the GeoLite2 country database into the same location
+# Download the IP-to-country MMDB database into the same location
 # used by Alpine's libmaxminddb package.
 
+# IP Geolocation by DB-IP (https://db-ip.com)
+
 TMPDIR="$(mktemp -d)"
-FILENAME="GeoLite2-Country.mmdb"
-LIBDIR="/var/lib/libmaxminddb"
-curl --silent "https://geolite.maxmind.com/download/geoip/database/$FILENAME.gz" -o "$TMPDIR/$FILENAME.gz"
+FILENAME="ip-country.mmdb"
+
+# We need to make sure that we grab an existing database at install-time
+for monthdelta in {0..10}; do
+    newdate=$(date --date="-$monthdelta month" +%Y-%m)
+    ADDRESS="https://download.db-ip.com/free/ip-country-${newdate}.mmdb.gz"
+    curl --fail --silent "${ADDRESS}" -o "$TMPDIR/$FILENAME.gz" > /dev/null && break
+    if (( monthdelta == 10 )); then
+        # A weird exit code on purpose -- we should catch this long before it triggers
+        exit 2
+    fi
+done
+
 gunzip "$TMPDIR/$FILENAME.gz"
+LIBDIR="/var/lib/libmaxminddb"
 mkdir -p $LIBDIR
 mv -f "$TMPDIR/$FILENAME" $LIBDIR
 rmdir $TMPDIR
--- src/shadowbox/server/outline_shadowsocks_server.ts
+++ src/shadowbox/server/outline_shadowsocks_server.ts
@@ -32,7 +32,7 @@ export class OutlineShadowsocksServer implements ShadowsocksServer {
       private readonly metricsLocation: string) {}
 
   // Annotates the Prometheus data metrics with countries.
-  // ipCountryFilename is the location of the GeoLite2-Country.mmdb file.
+  // ipCountryFilename is the location of the ip-country.mmdb IP-to-country database file.
   enableCountryMetrics(ipCountryFilename: string): OutlineShadowsocksServer {
     this.ipCountryFilename = ipCountryFilename;
     return this;
