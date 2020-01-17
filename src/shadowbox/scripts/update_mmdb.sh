1a33fefdfcb1e7826c5b5aae2ee7a67fa469c66b
Jon Cohen <cohenjon@google.com>
2020-01-17 17:17:23 -0500

    Fix update_mmdb and the Docker image to work on Alpine (#559)
    * Fix the for loop in update_mmdb to be `dash` compatible
    
    for x in {a..b} is bash-only, and alpine's default shell is `dash`.
    
    * Fix other issues related to using musl/Busybox/dash
    
    * Use `[[` instead of `((` for the branch
    * Make sure to update to GNU coreutils in the image in order to use
    `date --date`
    
    * Respond to review comments

--- src/shadowbox/scripts/update_mmdb.sh
+++ src/shadowbox/scripts/update_mmdb.sh
@@ -3,8 +3,25 @@
 # Download the GeoLite2 country database into the same location
 # used by Alpine's libmaxminddb package.
 
+# IP Geolocation by DB-IP (https://db-ip.com)
+
+# Note that this runs on BusyBox sh, which lacks bash features.
+
 TMPDIR="$(mktemp -d)"
-FILENAME="GeoLite2-Country.mmdb"
+FILENAME="ip-country.mmdb"
+
+# We need to make sure that we grab an existing database at install-time
+for monthdelta in $(seq 10); do
+    newdate=$(date --date="-$monthdelta months" +%Y-%m)
+    ADDRESS="https://download.db-ip.com/free/dbip-country-lite-${newdate}.mmdb.gz"
+    curl --fail --silent "${ADDRESS}" -o "$TMPDIR/$FILENAME.gz" > /dev/null && break  
+    if [[ $monthdelta -eq 10 ]]; then
+        # A weird exit code on purpose -- we should catch this long before it triggers
+        exit 2
+    fi
+done
+
+gunzip "$TMPDIR/$FILENAME.gz"
 LIBDIR="/var/lib/libmaxminddb"
 curl --silent "https://geolite.maxmind.com/download/geoip/database/$FILENAME.gz" -o "$TMPDIR/$FILENAME.gz"
 gunzip "$TMPDIR/$FILENAME.gz"
