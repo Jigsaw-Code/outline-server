769024e2248ce0c6ec67755f1091151959b4fc2c
Vinicius Fortuna <fortuna@users.noreply.github.com>
2020-04-28 01:43:02 -0400

    Remove unused url import

--- src/server_manager/web_app/main.ts
+++ src/server_manager/web_app/main.ts
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as url from 'url';
-
 import * as digitalocean_api from '../cloud/digitalocean_api';
 import * as i18n from '../infrastructure/i18n';
 
