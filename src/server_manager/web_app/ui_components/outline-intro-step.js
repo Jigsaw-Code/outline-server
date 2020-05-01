2b49812eb06e91ab678629ffe90e0e91b4895a40
Vinicius Fortuna <fortuna@users.noreply.github.com>
2020-05-01 19:15:05 -0400

    Fix comment

--- src/server_manager/web_app/ui_components/outline-intro-step.js
+++ src/server_manager/web_app/ui_components/outline-intro-step.js
@@ -21,6 +21,7 @@ import './outline-step-view.js';
 import './style.css';
 import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+
 Polymer({
   _template: html`
     <style include="cloud-install-styles"></style>
@@ -56,7 +57,7 @@ Polymer({
         cursor: pointer;
         box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.02), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
       }
-      /* Card shadows (common to all cards).\` */
+      /* Card shadows (common to all cards). */
       .card:hover {
         box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.1), 0 1px 10px 0 rgba(0, 0, 0, 0.2);
       }
