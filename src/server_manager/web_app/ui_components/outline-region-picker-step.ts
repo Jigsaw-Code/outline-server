72a83def4f95ba648acf758c473d64f819a8c0d8
Martin McRoy <mpmcroy@google.com>
2020-07-28 13:55:48 -0400

    Formatting

--- src/server_manager/web_app/ui_components/outline-region-picker-step.ts
+++ src/server_manager/web_app/ui_components/outline-region-picker-step.ts
@@ -112,28 +112,22 @@ export class OutlineRegionPicker extends LitElement {
       <span slot="step-title">${this.localize('region-title')}</span>
       <span slot="step-description">${this.localize('region-description')}</span>
       <span slot="step-action">
-        <paper-button id="createServerButton" @tap="${this._handleCreateServerTap}" ?disabled="${
-    !this._isCreateButtonEnabled(this.isServerBeingCreated, this.selectedLocationId)}">
+        <paper-button id="createServerButton" @tap="${this._handleCreateServerTap}" ?disabled="${!this._isCreateButtonEnabled(this.isServerBeingCreated, this.selectedLocationId)}">
           ${this.localize('region-setup')}
         </paper-button>
       </span>
       <div class="card-content" id="cityContainer">
         ${this.locations.map(item => {
-      return html`
-          <input type="radio" id="card-${item.id}" name="${item.id}" ?disabled="${
-      !item.available}" .checked="${
-          this._isLocationSelected(this.selectedLocationId,
-                                   item.id)}" @tap="${this._locationSelected}">
+          return html`
+          <input type="radio" id="card-${item.id}" name="${item.id}" ?disabled="${!item.available}" .checked="${this._isLocationSelected(this.selectedLocationId, item.id)}" @tap="${this._locationSelected}">
           <label for="card-${item.id}" class="city-button">
-            <iron-icon icon="check-circle" ?hidden="${
-      !this._isLocationSelected(this.selectedLocationId, item.id)}"></iron-icon>
+            <iron-icon icon="check-circle" ?hidden="${!this._isLocationSelected(this.selectedLocationId, item.id)}"></iron-icon>
             <img class="flag" src="${item.flag}">
             <div class="city-name">${item.name}</div>
           </label>`;
-    })}
+        })}
       </div>
-      <paper-progress .hidden="${
-    !this.isServerBeingCreated}" indeterminate="" class="slow"></paper-progress>
+      <paper-progress .hidden="${!this.isServerBeingCreated}" indeterminate="" class="slow"></paper-progress>
     </outline-step-view>
     `;
   }
