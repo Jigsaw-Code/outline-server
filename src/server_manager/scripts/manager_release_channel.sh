bd8c656183a79c77161f1c2b7a2a82e4bb4f7c51
Jonathan Cohen <cohenjon@google.com>
2020-09-24 16:38:56 -0400

    Clarify manager_release_channel.sh

--- src/server_manager/scripts/manager_release_channel.sh
+++ src/server_manager/scripts/manager_release_channel.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# If this isn't an alpha or beta build, `cut -s` will return an empty string
 INFO_FILE_CHANNEL=$(jq -r '.version' src/server_manager/package.json | cut -s -d'-' -f2)
 if [[ -z "${INFO_FILE_CHANNEL}" ]]; then
   INFO_FILE_CHANNEL=latest
