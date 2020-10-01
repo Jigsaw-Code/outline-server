a5d53bc2aade1d1d5ecbd922598c9deceb5e58b4
Jonathan Cohen <cohenjon@google.com>
2020-10-01 13:58:53 -0400

    Release channels and staged releases for the manager (#736)
    * Generate all updates files
    
    * TEST
    
    * Allow staged releases and beta channels for all Electron platforms
    
    * Clarify manager_release_channel.sh
    
    * Install jq in the CI build
    
    * Use a common script for common packaging options
    
    * Fix comment in get_packaging_opts
    
    * Use a version of `jq` managed by Yarn
    
    * Make staged rollouts affect the beta channel for mainline builds
    
    Also remove the alpha info chanel files since we aren't using them.
    
    * Fix packaging scripts when no args are passed
    
    * Don't install jq in Travis anymore
    
    * Fix trailing newlines
    
    * Fill packaging options by `source`-ing the options parsing script
    
    * Rename to get_manager_release_channel.sh

--- src/server_manager/electron_app/package_macos_action.sh
+++ src/server_manager/electron_app/package_macos_action.sh
@@ -14,15 +14,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+source src/server_manager/scripts/fill_packaging_opts.sh $0 $@
+
 yarn do server_manager/electron_app/build
+
+readonly BUILD_DIR=build/server_manager/electron_app/static
 
 # Produces dmg and zip images. The latter is required for auto-update.
 $ROOT_DIR/src/server_manager/node_modules/.bin/electron-builder \
-  --projectDir=build/server_manager/electron_app/static \
+  --projectDir="${BUILD_DIR}" \
   --config.asarUnpack=server_manager/web_app/images \
+  --config.generateUpdatesFilesForAllChannels=true \
   --publish=never \
   --config.publish.provider=generic \
   --config.publish.url=https://raw.githubusercontent.com/Jigsaw-Code/outline-releases/master/manager/ \
   --mac default \
   --config.mac.icon=icons/mac/icon.icns \
   --config.artifactName='Outline-Manager.${ext}'
+
+src/server_manager/scripts/finish_info_files.sh mac $STAGING_PERCENTAGE
---
+++ src/server_manager/scripts/finish_info_files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eu
+#
+# Copyright 2018 The Outline Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PLATFORM="-$1"
+if [[ "${PLATFORM}" = "-win" ]]; then
+  PLATFORM=""
+fi
+readonly STAGING_PERCENTAGE="${2}"
+readonly BUILD_DIR=build/server_manager/electron_app/static
+
+INFO_FILE_CHANNEL=$(src/server_manager/scripts/get_manager_release_channel.sh)
+echo "stagingPercentage: $STAGING_PERCENTAGE" >> "${BUILD_DIR}"/dist/"${INFO_FILE_CHANNEL}${PLATFORM}".yml
+
+# If we cut a staged mainline release, beta testers will take the update as well.
+if [[ "${INFO_FILE_CHANNEL}" = "latest" ]]; then
+  echo "stagingPercentage: $STAGING_PERCENTAGE" >> "${BUILD_DIR}"/dist/beta"${PLATFORM}".yml
+fi
+
+# We don't support alpha releases
+rm -f "${BUILD_DIR}"/dist/alpha"${PLATFORM}".yml
