87b7ef7f1cd93a70da6125dda235726299cf43d4
Jonathan Cohen <cohenjon@google.com>
2020-11-20 19:27:30 -0500

    Cleanup a stray typo

--- src/server_manager/web_app/shadowbox_server.ts
+++ src/server_manager/web_app/shadowbox_server.ts
@@ -32,7 +32,7 @@ export interface ServerConfig {
   portForNewAccessKeys: number;
   hostnameForAccessKeys: string;
   version: string;
-  // This is the server default data limit.  We use this instead of defaultDataLimit for API//
+  // This is the server default data limit.  We use this instead of defaultDataLimit for API
   // backwards compatibility.
   accessKeyDataLimit?: server.DataLimit;
 }
