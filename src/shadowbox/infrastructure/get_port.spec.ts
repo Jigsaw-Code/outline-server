fe1c30eb3835bbc89838be26781c42887e2e6afc
Jonathan Cohen <cohenjon@google.com>
2021-09-21 17:27:17 -0400

    Fix type errors

--- src/shadowbox/infrastructure/get_port.spec.ts
+++ src/shadowbox/infrastructure/get_port.spec.ts
@@ -33,8 +33,8 @@ describe('PortProvider', () => {
     it('returns free port', async () => {
       const ports = new get_port.PortProvider();
       const server = await listen();
-      expect(await ports.reserveFirstFreePort(server.address().port))
-          .toBeGreaterThan(server.address().port);
+      const getPort = (server: net.Server) => (server.address() as net.AddressInfo).port;
+      expect(await ports.reserveFirstFreePort(getPort(server))).toBeGreaterThan(getPort(server));
       server.close();
     });
 
