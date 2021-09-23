2fc05fdde091dd112a5808d6d889fb585a5e5647
Jonathan Cohen <cohenjon@google.com>
2021-09-23 16:04:38 -0400

    Fixup suggestion

--- src/shadowbox/infrastructure/get_port.spec.ts
+++ src/shadowbox/infrastructure/get_port.spec.ts
@@ -33,8 +33,8 @@ describe('PortProvider', () => {
     it('returns free port', async () => {
       const ports = new get_port.PortProvider();
       const server = await listen();
-      const initialPort = (s.address() as net.AddressInfo).port;
-      expect(await ports.reserveFirstFreePort(initialPort).toBeGreaterThan(initialPort);
+      const initialPort = (server.address() as net.AddressInfo).port;
+      expect(await ports.reserveFirstFreePort(initialPort)).toBeGreaterThan(initialPort);
       server.close();
     });
 
