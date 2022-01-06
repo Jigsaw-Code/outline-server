59f4321f356d9987ffa435673e921cceda8a4293
ValdikSS <iam@valdikss.org.ru>
2022-01-06 21:17:03 +0300

    Update src/server_manager/install_scripts/install_server.sh
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -158,10 +158,13 @@ function fetch() {
 }
 
 function install_docker() {
-  umask 0022
-  (chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg 2>/dev/null || true)
-  (fetch https://get.docker.com/ | sh) >&2
-  umask 0007
+  (
+    # Change umask so that /usr/share/keyrings/docker-archive-keyring.gpg has the right permissions.
+    # See https://github.com/Jigsaw-Code/outline-server/issues/951.
+    # We do this in a subprocess so the umask for the calling process is unaffected.
+    umask 0022
+    fetch https://get.docker.com/ | sh
+  ) >&2
 }
 
 function start_docker() {
