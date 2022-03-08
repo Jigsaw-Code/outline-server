5a4966cf7c47e10a850cd03109be497c60282d4f
Daniel LaCosse <d@niellacos.se>
2022-03-08 14:01:08 -0500

    feat(release): manager debug workflow (#1055)
    * basic workflow sketch
    
    * metrics server also needs lint
    
    * update names
    
    * latest?
    
    * that's weird.
    
    * ok that's a start. refactor next
    
    * fix path
    
    * oops
    
    * ah
    
    * no op
    
    * make it like the other ones
    
    * refactor scripts
    
    * fix shellcheck
    
    * ah okay
    
    * upload mac artifact
    
    * Update .travis.yml
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>
    
    * comments
    
    * add todo
    
    Co-authored-by: Vinicius Fortuna <fortuna@users.noreply.github.com>

--- src/server_manager/electron_app/build.action.sh
+++ src/server_manager/electron_app/build.action.sh
@@ -13,8 +13,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -eux
 
-# Builds the Electron App
+PLATFORM=$1
+STAGING_PERCENTAGE=100
+BUILD_MODE=debug
+for i in "$@"; do
+  case ${i} in
+  --buildMode=*)
+    BUILD_MODE="${i#*=}"
+    shift
+    ;;
+  --stagingPercentage=*)
+    STAGING_PERCENTAGE="${i#*=}"
+    shift
+    ;;
+  --* | -*)
+    echo "Unknown option: ${i}"
+    exit 1
+    ;;
+  *) ;;
+  esac
+done
 
 readonly OUT_DIR="${BUILD_DIR}/server_manager/electron_app"
 rm -rf "${OUT_DIR}"
@@ -40,8 +60,14 @@ cp -r "${BUILD_DIR}/server_manager/web_app/static" "${STATIC_DIR}/server_manager
 # is loaded via a custom protocol.
 cp src/server_manager/package.json package-lock.json "${STATIC_DIR}"
 cd "${STATIC_DIR}"
-npm install --prod --ignore-scripts
+npm ci --prod --ignore-scripts
 
 # Icons.
 cd "${ROOT_DIR}"
 electron-icon-maker --input=src/server_manager/images/launcher-icon.png --output=build/server_manager/electron_app/static
+
+# TODO(daniellacosse): refactor these scripts into node so we can call the electron builder there directly
+# shellcheck disable=SC2046
+electron-builder $(node src/server_manager/scripts/get_electron_build_flags.mjs "${PLATFORM}" --buildMode "${BUILD_MODE}")
+
+src/server_manager/scripts/finish_info_files.sh "${PLATFORM}" "${STAGING_PERCENTAGE}"
