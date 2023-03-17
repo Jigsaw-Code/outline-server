8e3d73b66f83c53044a4f25951d653b4bac724b2
Vinicius Fortuna <fortuna@users.noreply.github.com>
2023-03-17 14:38:29 -0600

    Update src/server_manager/install_scripts/install_server.sh

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -171,7 +171,8 @@ function start_docker() {
   if command -v systemctl &> /dev/null; then
     systemctl enable --now docker.service >&2
     service docker start
-  else #dirty hack to support Alpine Linux
+  else
+    # Hack to support Alpine Linux
     rc-update add docker boot
     rc-service docker start
   fi
