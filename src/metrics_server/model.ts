289d0b8e06a998d2595cb25a01e4c68c38d74e2d
Abdelaziz Mohamed Mousa Ahmed <aziztrade@outlook.com>
2023-05-13 16:49:40 +0300

    Update model.ts
    Here are the changes I made to the code:
    
    I made the code more human-centered by using more natural language and by adding more comments.
    I added documentation to the code by adding a docstring to each interface.
    I added a summary of the changes I made to the code.

--- src/metrics_server/model.ts
+++ src/metrics_server/model.ts
@@ -14,27 +14,88 @@
 
 // NOTE: These interfaces are mirrored in in src/shadowbox/server/metrics.ts
 // Find a way to share them between shadowbox and metrics_server.
+
+/**
+ * An interface for representing hourly connection metrics reports.
+ */
 export interface HourlyConnectionMetricsReport {
+  /**
+   * The ID of the server that generated the report.
+   */
   serverId: string;
+
+  /**
+   * The start time of the report, in milliseconds since the Unix epoch.
+   */
   startUtcMs: number;
+
+  /**
+   * The end time of the report, in milliseconds since the Unix epoch.
+   */
   endUtcMs: number;
+
+  /**
+   * An array of reports for each user who connected to the server during the report period.
+   */
   userReports: HourlyUserConnectionMetricsReport[];
 }
 
+/**
+ * An interface for representing hourly user connection metrics reports.
+ */
 export interface HourlyUserConnectionMetricsReport {
+  /**
+   * The ID of the user who connected to the server.
+   */
   userId: string;
+
+  /**
+   * An array of countries that the user connected from.
+   */
   countries: string[];
+
+  /**
+   * The total number of bytes that the user transferred during the report period.
+   */
   bytesTransferred: number;
 }
 
+/**
+ * An interface for representing daily feature metrics reports.
+ */
 export interface DailyFeatureMetricsReport {
+  /**
+   * The ID of the server that generated the report.
+   */
   serverId: string;
+
+  /**
+   * The version of the server that generated the report.
+   */
   serverVersion: string;
+
+  /**
+   * The timestamp of the report, in milliseconds since the Unix epoch.
+   */
   timestampUtcMs: number;
+
+  /**
+   * A report for the data limit for the server.
+   */
   dataLimit: DailyDataLimitMetricsReport;
 }
 
+/**
+ * An interface for representing daily data limit metrics reports.
+ */
 export interface DailyDataLimitMetricsReport {
+  /**
+   * Whether or not the data limit is enabled.
+   */
   enabled: boolean;
+
+  /**
+   * The number of keys that have been used against the data limit.
+   */
   perKeyLimitCount?: number;
 }
