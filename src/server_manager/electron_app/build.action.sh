0b52af1d55e846ecf5bc207fc7a838a8a937eabb
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2023-05-30 15:06:53 -0400

    ah?

--- src/server_manager/electron_app/build.action.sh
+++ src/server_manager/electron_app/build.action.sh
@@ -70,7 +70,7 @@ cp -r "${BUILD_DIR}/server_manager/web_app/static" "${STATIC_DIR}/server_manager
 # is loaded via a custom protocol.
 cp src/server_manager/package.json package-lock.json "${STATIC_DIR}"
 cd "${STATIC_DIR}"
-sed -i '' "s/0.0.0-debug/${VERSION_NAME}/g" package.json
+sed -i '' "s/0.0.0-debug/${VERSION_NAME}/g" src/server_manager/package.json
 npm ci --prod --ignore-scripts
 
 # Icons.
