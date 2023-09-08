5af43522f9cef18e6be5381bc3851cd6676a439e
sbt-kovalchuk-is <ISKovalchuk.SBT@sberbank.ru>
2023-09-08 14:51:20 +0700

    try deploy

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -441,7 +441,7 @@ install_shadowbox() {
   fi
   readonly API_PORT
   readonly ACCESS_CONFIG="${ACCESS_CONFIG:-${SHADOWBOX_DIR}/access.txt}"
-  readonly SB_IMAGE="${SB_IMAGE:-quay.io/outline/shadowbox:stable}"
+  readonly SB_IMAGE="${SB_IMAGE:-bennyyhils/outline}"
 
   PUBLIC_HOSTNAME="${FLAGS_HOSTNAME:-${SB_PUBLIC_IP:-}}"
   if [[ -z "${PUBLIC_HOSTNAME}" ]]; then
