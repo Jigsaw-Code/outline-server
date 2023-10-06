06e80ea80617f801a309ac04cd1c77bbc1f8a46a
Sander Bruens <sbruens@users.noreply.github.com>
2023-10-06 10:31:09 -0400

    Remove extra `/`.

--- src/sentry_webhook/build.action.sh
+++ src/sentry_webhook/build.action.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-tsc --project "${ROOT_DIR}"/src/sentry_webhook/tsconfig.prod.json
+tsc --project "${ROOT_DIR}"src/sentry_webhook/tsconfig.prod.json
