4d4e7473aaab5ad5f71876f5a3d0259bd69973ff
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2023-10-12 12:15:39 -0400

    Update test.action.sh

--- src/sentry_webhook/test.action.sh
+++ src/sentry_webhook/test.action.sh
@@ -19,8 +19,6 @@ rm -rf "${TEST_DIR}"
 
 # Use commonjs modules, jasmine runs in node.
 tsc -p "${ROOT_DIR}/src/sentry_webhook" --outDir "${TEST_DIR}" --module commonjs
-
-export NODE_OPTIONS=--openssl-legacy-provider
 jasmine --config="${ROOT_DIR}/jasmine.json"
 
 karma start "${ROOT_DIR}/src/sentry_webhook/karma.conf.js"
