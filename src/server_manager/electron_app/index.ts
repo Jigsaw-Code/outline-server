41d498f2281bb08f2d972538a21154152bb44f1f
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2023-11-17 11:48:58 -0500

    fix(manager/electron): separate main and renderer sentry libraries (#1451)

--- src/server_manager/electron_app/index.ts
+++ src/server_manager/electron_app/index.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as Sentry from '@sentry/electron';
+import * as Sentry from '@sentry/electron/main';
 import * as dotenv from 'dotenv';
 import * as electron from 'electron';
 import {autoUpdater} from 'electron-updater';
