2efd02ef9170785f3a80cd98cd17d7286970360d
Sander Bruens <sbruens@users.noreply.github.com>
2023-11-20 11:36:20 -0500

    Log response header at DEBUG level.

--- src/sentry_webhook/post_sentry_event_to_salesforce.ts
+++ src/sentry_webhook/post_sentry_event_to_salesforce.ts
@@ -108,7 +108,7 @@ export function postSentryEventToSalesforce(event: SentryEvent, project: string)
       },
       (res) => {
         if (res.statusCode === 200) {
-          console.log('Salesforce `is-processed`:', res.headers['is-processed']);
+          console.debug('Salesforce `is-processed`:', res.headers['is-processed']);
           resolve();
         } else {
           reject(new Error(`Failed to post form data, response status: ${res.statusCode}`));
