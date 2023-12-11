1bbb9db152ce99ed36a9afa4bf27391ff91f8cc2
Sander Bruens <sbruens@users.noreply.github.com>
2023-12-11 15:25:32 -0500

    Keep the dev changes as they don't have a production impact.

--- src/sentry_webhook/post_sentry_event_to_salesforce.ts
+++ src/sentry_webhook/post_sentry_event_to_salesforce.ts
@@ -35,7 +35,7 @@ interface SalesforceFormValues {
   recordType: string;
 }
 
-const SALESFORCE_DEV_HOST = 'google-jigsaw--uat.my.salesforce.com';
+const SALESFORCE_DEV_HOST = 'google-jigsaw--jigsawuat.sandbox.my.salesforce.com';
 const SALESFORCE_PROD_HOST = 'webto.salesforce.com';
 const SALESFORCE_PATH = '/servlet/servlet.WebToCase';
 const SALESFORCE_FORM_FIELDS_DEV: SalesforceFormFields = {
@@ -63,7 +63,7 @@ const SALESFORCE_FORM_FIELDS_PROD: SalesforceFormFields = {
   type: 'type',
 };
 const SALESFORCE_FORM_VALUES_DEV: SalesforceFormValues = {
-  orgId: '00D3F000000DDDH',
+  orgId: '00D750000004dFg',
   recordType: '0123F000000MWTS',
 };
 const SALESFORCE_FORM_VALUES_PROD: SalesforceFormValues = {
