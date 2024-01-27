defeb926c62ee6d9d9b708ccf545fadc6acf3469
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-01-27 05:22:35 +0000

    Fix integration

--- src/shadowbox/integration_test/run.action.sh
+++ src/shadowbox/integration_test/run.action.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# run_action shadowbox/docker/build
+run_action shadowbox/docker/build
 
 LOGFILE="$(mktemp)"
 readonly LOGFILE
