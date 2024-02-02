8930831faeed78bbccfba7120aab987fc3b00e7a
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-02-02 11:33:03 -0500

    Add -f

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -311,7 +311,7 @@ function start_shadowbox() {
 set -eu
 
 docker stop "${CONTAINER_NAME}" 2> /dev/null || true
-docker rm "${CONTAINER_NAME}" 2> /dev/null || true
+docker rm -f "${CONTAINER_NAME}" 2> /dev/null || true
 
 docker_command=(
   docker
