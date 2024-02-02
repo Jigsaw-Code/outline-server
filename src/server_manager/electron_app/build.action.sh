28d2b2e391ab8973173eb344102dd5834c7388a3
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-02-02 17:50:19 -0500

    chore(manager): cleanup and fix Manager build (#1497)

--- src/server_manager/electron_app/build.action.sh
+++ src/server_manager/electron_app/build.action.sh
@@ -13,10 +13,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -eux
+set -eu
 
-PLATFORM=$1
-STAGING_PERCENTAGE=100
+# Electron app root folder
+readonly STATIC_DIR="${BUILD_DIR}/server_manager/electron_app/static"
+rm -rf "${STATIC_DIR}"
+mkdir -p "${STATIC_DIR}"
+
 VERSION_NAME='0.0.0-debug'
 BUILD_MODE=debug
 for i in "$@"; do
@@ -29,10 +32,6 @@ for i in "$@"; do
     VERSION_NAME="${i#*=}"
     shift
     ;;
-  --stagingPercentage=*)
-    STAGING_PERCENTAGE="${i#*=}"
-    shift
-    ;;
   --* | -*)
     echo "Unknown option: ${i}"
     exit 1
@@ -41,12 +40,14 @@ for i in "$@"; do
   esac
 done
 
-readonly OUT_DIR="${BUILD_DIR}/server_manager/electron_app"
-rm -rf "${OUT_DIR}"
-
-# Electron app root folder
-readonly STATIC_DIR="${OUT_DIR}/static"
-mkdir -p "${STATIC_DIR}"
+if [[ -z "${WEBPACK_MODE:-}" ]]; then
+  case "${BUILD_MODE}" in
+    release)
+      export WEBPACK_MODE="production";;
+    *)
+      export WEBPACK_MODE="development";;
+  esac
+fi
 
 # Build the Web App.
 run_action server_manager/web_app/build
@@ -54,15 +55,12 @@ run_action server_manager/web_app/build
 # Compile the Electron main process and preload to the app root folder.
 # Since Node.js on Cygwin doesn't like absolute Unix-style paths,
 # we'll use relative paths here.
-webpack --config=src/server_manager/electron_main.webpack.mjs ${BUILD_ENV:+--mode=${BUILD_ENV}}
-webpack --config=src/server_manager/electron_preload.webpack.mjs ${BUILD_ENV:+--mode=${BUILD_ENV}}
+webpack --config=src/server_manager/electron_main.webpack.mjs ${WEBPACK_MODE:+--mode=${WEBPACK_MODE}}
+webpack --config=src/server_manager/electron_preload.webpack.mjs ${WEBPACK_MODE:+--mode=${WEBPACK_MODE}}
 
 # Assemble everything together.
 mkdir -p "${STATIC_DIR}/server_manager"
 cp -r "${BUILD_DIR}/server_manager/web_app/static" "${STATIC_DIR}/server_manager/web_app/"
-
-# TODO(fortuna): Separate the build of Electron main and the Electron package.
-# Building the package significantly delays the start action.
 
 # Electron requires a package.json file for the app's name, etc.
 # We also need to install NPMs at this location for require()
@@ -76,9 +74,3 @@ npm ci --prod --ignore-scripts
 # Icons.
 cd "${ROOT_DIR}"
 electron-icon-maker --input=src/server_manager/images/launcher-icon.png --output=build/server_manager/electron_app/static
-
-# TODO(daniellacosse): refactor these scripts into node so we can call the electron builder there directly
-# shellcheck disable=SC2046
-electron-builder $(node src/server_manager/scripts/get_electron_build_flags.mjs "${PLATFORM}" --buildMode "${BUILD_MODE}")
-
-src/server_manager/scripts/finish_info_files.sh "${PLATFORM}" "${STAGING_PERCENTAGE}"
