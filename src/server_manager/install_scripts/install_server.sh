0afb1273a6d8ea3d6cc009b245261bf7cfff5a4a
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-02-05 09:52:13 -0500

    Only escape what is needed.

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -551,20 +551,19 @@ function escape_json_string() {
     local char="${input:i:1}"
     local escaped="${char}"
     case "${char}" in
-        # List from https://www.json.org/.
-        $'"' ) escaped="\\\"";;
-        $'\\') escaped="\\\\";;
-        $'/' ) escaped="\\/";;
-        $'\b') escaped="\\b";;
-        $'\f') escaped="\\f";;
-        $'\n') escaped="\\n";;
-        $'\r') escaped="\\r";;
-        $'\t') escaped="\\t";;
-        *) 
-          if [[ "${char}" < $'\x20' ]]; then
-            escaped=$(printf "\u%04X" "'${char}")
-          fi
-          ;;
+      $'"' ) escaped="\\\"";;
+      $'\\') escaped="\\\\";;
+      *)
+        if [[ "${char}" < $'\x20' ]]; then
+          case "${char}" in 
+            $'\b') escaped="\\b";;
+            $'\f') escaped="\\f";;
+            $'\n') escaped="\\n";;
+            $'\r') escaped="\\r";;
+            $'\t') escaped="\\t";;
+            *) escaped=$(printf "\u%04X" "'${char}")
+          esac
+        fi;;
     esac
     echo -n "${escaped}"
   done
